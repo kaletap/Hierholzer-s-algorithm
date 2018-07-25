d5bc88c48c8c8df9d9d1744628b1cfacca678299
Przemyslaw2889 <kaletap@student.mini.pw.edu.pl>
2018-07-25 20:55:56 +0200

    cmake files created by Clion

---
+++ cmake-build-debug/CMakeFiles/heirholzers_algorithm.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Kira/CLionProjects/hierholzers-algoritm/graph/Graph.cpp" "C:/Users/Kira/CLionProjects/hierholzers-algoritm/cmake-build-debug/CMakeFiles/heirholzers_algorithm.dir/graph/Graph.cpp.obj"
+  "C:/Users/Kira/CLionProjects/hierholzers-algoritm/main.cpp" "C:/Users/Kira/CLionProjects/hierholzers-algoritm/cmake-build-debug/CMakeFiles/heirholzers_algorithm.dir/main.cpp.obj"
+  "C:/Users/Kira/CLionProjects/hierholzers-algoritm/node/Node.cpp" "C:/Users/Kira/CLionProjects/hierholzers-algoritm/cmake-build-debug/CMakeFiles/heirholzers_algorithm.dir/node/Node.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../node"
+  "../graph"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/heirholzers_algorithm.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/heirholzers_algorithm.dir/main.cpp.obj"
+  "CMakeFiles/heirholzers_algorithm.dir/node/Node.cpp.obj"
+  "CMakeFiles/heirholzers_algorithm.dir/graph/Graph.cpp.obj"
+  "heirholzers_algorithm.pdb"
+  "heirholzers_algorithm.exe"
+  "heirholzers_algorithm.exe.manifest"
+  "heirholzers_algorithm.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/heirholzers_algorithm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
